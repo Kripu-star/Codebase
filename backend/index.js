13955f6f6849287601abf411d8e259788828f86b
RohitRahulDey <gamergenix0987@gmail.com>
2024-12-11 19:24:20 +0530

    server serves cliint

--- backend/index.js
+++ backend/index.js
@@ -9,17 +9,20 @@ import DashboardRoutes from './routes/DashBoard.js';
 // import CommentRoutes from './routes/Comments.js';  // Uncomment if needed
 import PublicRoutes from './routes/public.js';
 import Router from './routes/filter.js';
-
+import path from 'path'
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 DBCon();
+
+const _dirname=path.resolve();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get('/', (req, res) => {
-    res.send('hello from server');
-});
+// app.get('/', (req, res) => {
+//     res.send('hello from server');
+// });
 app.use(express.static('public'));
 app.use(cookieParser());
 
@@ -37,6 +40,14 @@ app.use('/dashboard', DashboardRoutes);
 app.use('/public', PublicRoutes);
 
 app.use('/user', Router);  // `Router` is being used here for the `/filter` route
+
+
+//serving clint using server
+app.use(express.static(path.join(_dirname,"/frontend/dist")));
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
+});
+
 
 app.listen(PORT, () => {
     console.log(`App is running on Port ${PORT}`);
